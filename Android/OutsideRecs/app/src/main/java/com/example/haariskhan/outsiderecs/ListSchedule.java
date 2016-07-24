f499a8394223d5159c01505a699c4f49009c8f9e
huma khan <humakhan@humas-MacBook-Air.local>
2016-07-24 01:48:10 -0700

    Kaee's modulee working, basic rows with text working, installing JAR to handle images to ImageView through URL

--- Android/OutsideRecs/app/src/main/java/com/example/haariskhan/outsiderecs/ListSchedule.java
+++ Android/OutsideRecs/app/src/main/java/com/example/haariskhan/outsiderecs/ListSchedule.java
@@ -3,7 +3,7 @@ package com.example.haariskhan.outsiderecs;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.ListView;
 
@@ -11,9 +11,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyCallback;
+import kaaes.spotify.webapi.android.SpotifyError;
 import kaaes.spotify.webapi.android.SpotifyService;
 import kaaes.spotify.webapi.android.models.Artist;
 import kaaes.spotify.webapi.android.models.Pager;
+import kaaes.spotify.webapi.android.models.PlaylistTrack;
 import retrofit.Callback;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
@@ -34,7 +37,7 @@ public class ListSchedule extends AppCompatActivity {
         String token = intent.getStringExtra("Token");
 
         img = (ImageView) findViewById(R.id.niceMeme);
-        listView = (ListView) findViewById(R.id.listView);
+        listView = (ListView) findViewById(R.id.artistList);
 
         api = new SpotifyApi();
         api.setAccessToken(token);
@@ -46,7 +49,19 @@ public class ListSchedule extends AppCompatActivity {
     }
 
     public void analyzeArtists() {
-        Pager<Artist> payload = service.getTopArtists();
-        System.out.println(payload.toString());
+
+        service.getTopArtists(new SpotifyCallback<Pager<Artist>>() {
+            @Override
+            public void success(Pager<Artist> artistPager, Response response) {
+                for (Artist a : artistPager.items) {
+                    System.out.println(a.name);
+                }
+            }
+
+            @Override
+            public void failure(SpotifyError error) {
+                System.out.println("Failure: " + error.getMessage());
+            }
+        });
     }
 }
