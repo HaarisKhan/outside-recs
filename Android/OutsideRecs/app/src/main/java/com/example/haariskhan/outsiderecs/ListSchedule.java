91323459374347badaaf3bab52fe248136100a9e
huma khan <humakhan@humas-MacBook-Air.local>
2016-07-23 21:04:49 -0700

    Finished basic login page and authentication, generating recommendations now

---
+++ Android/OutsideRecs/app/src/main/java/com/example/haariskhan/outsiderecs/ListSchedule.java
@@ -0,0 +1,52 @@
+package com.example.haariskhan.outsiderecs;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Artist;
+import kaaes.spotify.webapi.android.models.Pager;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+
+public class ListSchedule extends AppCompatActivity {
+
+    SpotifyApi api;
+    SpotifyService service;
+    ImageView img;
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_schedule);
+        Intent intent = getIntent();
+        String token = intent.getStringExtra("Token");
+
+        img = (ImageView) findViewById(R.id.niceMeme);
+        listView = (ListView) findViewById(R.id.listView);
+
+        api = new SpotifyApi();
+        api.setAccessToken(token);
+
+        service = api.getService();
+
+        analyzeArtists();
+
+    }
+
+    public void analyzeArtists() {
+        Pager<Artist> payload = service.getTopArtists();
+        System.out.println(payload.toString());
+    }
+}
---
+++ Android/OutsideRecs/app/src/main/java/com/example/haariskhan/outsiderecs/LoginActivity.java
@@ -0,0 +1,112 @@
+package com.example.haariskhan.outsiderecs;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+import com.spotify.sdk.android.player.ConnectionStateCallback;
+import com.spotify.sdk.android.player.Player;
+import com.spotify.sdk.android.player.PlayerNotificationCallback;
+import com.spotify.sdk.android.player.PlayerState;
+import com.spotify.sdk.android.player.Spotify;
+
+public class LoginActivity extends AppCompatActivity implements PlayerNotificationCallback, ConnectionStateCallback {
+
+    private static final int REQUEST_CODE = 1;
+    private static final String CLIENT_ID = "7d0b7eed7dcc4d0a91bd90663aef8e3e";
+    private static final String REDIRECT_URI = "outsiderecs://callback";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Intent intent = getIntent();
+
+        AuthenticationRequest.Builder builder =
+                new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
+
+        builder.setScopes(new String[]{"user-top-read", "playlist-modify-private"});
+        AuthenticationRequest request = builder.build();
+
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        // Check if result comes from the correct activity
+        String token = null;
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+            switch (response.getType()) {
+                // Response was successful and contains auth token
+                case TOKEN:
+                    // Handle successful response
+                    token = response.getAccessToken();
+                    break;
+
+                // Auth flow returned an error
+                case ERROR:
+                    // Handle error response
+                    break;
+
+                // Most likely auth flow was cancelled
+                default:
+                    // Handle other cases
+            }
+        }
+
+        if (!(token == null)) {
+            Intent intent1 = new Intent(this, ListSchedule.class);
+            intent1.putExtra("Token", token);
+            startActivity(intent1);
+        }
+    }
+
+    @Override
+    public void onLoggedIn() {
+        Log.d("MainActivity", "User logged in");
+    }
+
+    @Override
+    public void onLoggedOut() {
+        Log.d("MainActivity", "User logged out");
+    }
+
+    @Override
+    public void onLoginFailed(Throwable error) {
+        Log.d("MainActivity", "Login failed");
+    }
+
+    @Override
+    public void onTemporaryError() {
+        Log.d("MainActivity", "Temporary error occurred");
+    }
+
+    @Override
+    public void onConnectionMessage(String message) {
+        Log.d("MainActivity", "Received connection message: " + message);
+    }
+
+    @Override
+    public void onPlaybackEvent(PlayerNotificationCallback.EventType eventType, PlayerState playerState) {
+        Log.d("MainActivity", "Playback event received: " + eventType.name());
+    }
+
+    @Override
+    public void onPlaybackError(PlayerNotificationCallback.ErrorType errorType, String errorDetails) {
+        Log.d("MainActivity", "Playback error received: " + errorType.name());
+    }
+
+    @Override
+    protected void onDestroy() {
+        Spotify.destroyPlayer(this);
+        super.onDestroy();
+    }
+}
+
